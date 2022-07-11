a0ea8dc202a88b6d14eae03e56fab560a40b2f93
tika <48658947+tika@users.noreply.github.com>
2022-07-11 11:29:52 +0100

    feat: add redis in order to solve some of the loading time

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # tika.is
+
 the creations of a 16-year-old
 
 ### Technologies used
-- Next.js
-- Styled Components
+
+-   Next.js
+-   TailwindCSS
+-   Lanyard
--- src/pages/api/crucial.ts
+++ src/pages/api/crucial.ts
@@ -1,18 +1,35 @@
 import { createEndpoint } from "../../lib/endpoint";
 import axios from "axios";
+import redis from "../../lib/redis";
 
 export default createEndpoint<CrucialData>({
     GET: async (req, res) => {
         let repos: Repo[] = [];
         let page = 1;
 
-        const colors = await axios
-            .get(
-                "https://raw.githubusercontent.com/ozh/github-colors/master/colors.json"
-            )
-            .then(({ data }) => data);
+        // get github language colors from ozh's repo
+        let colors = await redis.get("colors");
 
+        if (!colors) {
+            colors = await getLanguageColors();
+
+            // updates every day -> now we don't have to refetch!
+            redis.set("colors", JSON.stringify(colors), "EX", 60 * 60 * 24);
+        }
+
+        // if i've already got all the info, then there's no point in refetching
+        // sidenote: not entirely sure if this is "good" design, but it works
+        const recent = await redis.get("data");
+
+        if (recent) {
+            return res.send(JSON.parse(recent));
+        }
+
+        // -- GET DATA --
+
+        // this basically gets every repo that I have, because GitHub won't let me get ALL repos at once
         do {
+            // get 100 repos
             await axios
                 .get(
                     `https://api.github.com/user/repos?per_page=100&page=${page}`,
@@ -23,10 +40,13 @@ export default createEndpoint<CrucialData>({
                     }
                 )
                 .then(({ data }) => {
+                    // GitHub associates ANY repo that i've touched with me so I only want the repos I own
+                    // nb: in future this might change, due to joint ownership and stuff
                     const myRepos = data.filter(
                         (it: any) => it.owner.login === "tika"
                     );
 
+                    // join the repos to the list we already
                     repos = repos.concat(
                         myRepos.map((it: any) => toRepo(it, colors))
                     );
@@ -39,15 +59,28 @@ export default createEndpoint<CrucialData>({
             (repo) => repo.visibility === "private"
         ).length;
 
-        res.send({
+        const data = {
             repos,
             privateRepoCount,
             publicRepoCount: repos.length - privateRepoCount,
-        });
+        };
+
+        // updates every 30 mins
+        redis.set("data", JSON.stringify(data), "EX", 60 * 30);
+
+        res.send(data);
     },
 });
 
-export function toRepo(x: GithubRepository, colors: any): Repo {
+function getLanguageColors() {
+    return axios
+        .get(
+            "https://raw.githubusercontent.com/ozh/github-colors/master/colors.json"
+        )
+        .then(({ data }) => data);
+}
+
+function toRepo(x: GithubRepository, colors: any): Repo {
     return {
         name: x.full_name
             .split("/")
